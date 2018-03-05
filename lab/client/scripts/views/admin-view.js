1ab8dcc53a09971498fcdae9bbff0313c879e8a5
Kris Sakarias <ksakarias@gmail.com>
2018-03-04 19:32:12 -0800

    finished lab

--- lab/client/scripts/views/admin-view.js
+++ lab/client/scripts/views/admin-view.js
@@ -13,7 +13,8 @@ var app = app || {};
       event.preventDefault();
       let token = event.target.passphrase.value;
 
-      // COMMENT: Is the token cleared out of local storage? Do you agree or disagree with this structure?
+      // COMMENT-DONE: Is the token cleared out of local storage? Do you agree or disagree with this structure?
+      // Yes, this get request is accompanied by the user entered passcode, and the server checks if it is the same as the token, which is being stored as an environment variable off of the client. It then calls the verify function below to check if login is true or false to show/hide main content.
       $.get(`${__API_URL__}/api/v1/admin`, {token})
         .then(res => {
           localStorage.token = true;
--- lab/server/server.js
+++ lab/server/server.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 // Application dependencies
 const express = require('express');
@@ -13,7 +13,8 @@ const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
 const TOKEN = process.env.TOKEN;
 
-// COMMENT: Explain the following line of code. What is the API_KEY? Where did it come from?
+// COMMENT-DONE: Explain the following line of code. What is the API_KEY? Where did it come from?
+// The API key is used to access data from the API being used, in this case the Google Books API. It is declared here as an environment variable to keep the API key private.
 const API_KEY = process.env.GOOGLE_API_KEY;
 
 // Database Setup
@@ -25,30 +26,35 @@ client.on('error', err => console.error(err));
 app.use(cors());
 
 // API Endpoints
-app.get('/api/v1/admin', (req, res) => res.send(TOKEN === parseInt(req.query.token)))
+app.get('/api/v1/admin', (req, res) => res.send(TOKEN === parseInt(req.query.token)));
 
 app.get('/api/v1/books/find', (req, res) => {
   let url = 'https://www.googleapis.com/books/v1/volumes';
 
-  // COMMENT: Explain the following four lines of code. How is the query built out? What information will be used to create the query?
-  let query = ''
+  // COMMENT-DONE: Explain the following four lines of code. How is the query built out? What information will be used to create the query?
+  // This code is building a query string that will be used to query the google books api by title, author, and isbn. If the request contains a title, it will add the title to the query string, and so on.
+  let query = '';
   if(req.query.title) query += `+intitle:${req.query.title}`;
   if(req.query.author) query += `+inauthor:${req.query.author}`;
   if(req.query.isbn) query += `+isbn:${req.query.isbn}`;
 
-  // COMMENT: What is superagent? How is it being used here? What other libraries are available that could be used for the same purpose?
+  // COMMENT-DONE: What is superagent? How is it being used here? What other libraries are available that could be used for the same purpose?
+  // superagent is a node module that helps us query external apis from the server and send the data back to the client. In this case it is taking the query string from above and the url and API key to query the API, map over the response, and return an object with the appropriate data.
   superagent.get(url)
     .query({'q': query})
     .query({'key': API_KEY})
     .then(response => response.body.items.map((book, idx) => {
 
-      // COMMENT: The line below is an example of destructuring. Explain destructuring in your own words.
+      // COMMENT-DONE: The line below is an example of destructuring. Explain destructuring in your own words.
+      // Destructuring is done on data that is sent to the server, and is a method of harvesting the desired properties and creating an object from those properties.
       let { title, authors, industryIdentifiers, imageLinks, description } = book.volumeInfo;
 
-      // COMMENT: What is the purpose of the following placeholder image?
+      // COMMENT-DONE: What is the purpose of the following placeholder image?
+      // If no images are sent back in the response, the placeholder image will be included as the image_url
       let placeholderImage = 'http://www.newyorkpaddy.com/images/covers/NoCoverAvailable.jpg';
 
-      // COMMENT: Explain how ternary operators are being used below.
+      // COMMENT-DONE: Explain how ternary operators are being used below.
+      // An object is returned for each data set, with the properties listed below. The ternary operator is used to easily assign values to a property depending on whether or not it exists/
       return {
         title: title ? title : 'No title available',
         author: authors ? authors[0] : 'No authors available',
@@ -56,13 +62,14 @@ app.get('/api/v1/books/find', (req, res) => {
         image_url: imageLinks ? imageLinks.smallThumbnail : placeholderImage,
         description: description ? description : 'No description available',
         book_id: industryIdentifiers ? `${industryIdentifiers[0].identifier}` : '',
-      }
+      };
     }))
     .then(arr => res.send(arr))
-    .catch(console.error)
-})
+    .catch(console.error);
+});
 
-// COMMENT: How does this route differ from the route above? What does ':isbn' refer to in the code below?
+// COMMENT-DONE: How does this route differ from the route above? What does ':isbn' refer to in the code below?
+// This route is being used to search for books only by ISBN, which comes with the request as a parameter. 
 app.get('/api/v1/books/find/:isbn', (req, res) => {
   let url = 'https://www.googleapis.com/books/v1/volumes';
   superagent.get(url)
@@ -78,11 +85,11 @@ app.get('/api/v1/books/find/:isbn', (req, res) => {
         isbn: industryIdentifiers ? `ISBN_13 ${industryIdentifiers[0].identifier}` : 'No ISBN available',
         image_url: imageLinks ? imageLinks.smallThumbnail : placeholderImage,
         description: description ? description : 'No description available',
-      }
+      };
     }))
     .then(book => res.send(book[0]))
-    .catch(console.error)
-})
+    .catch(console.error);
+});
 
 app.get('/api/v1/books', (req, res) => {
   client.query(`SELECT book_id, title, author, image_url, isbn FROM books;`)
@@ -100,10 +107,10 @@ app.post('/api/v1/books', bodyParser, (req, res) => {
   let {title, author, isbn, image_url, description} = req.body;
   client.query(`
     INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5)`,
-    [title, author, isbn, image_url, description]
+  [title, author, isbn, image_url, description]
   )
-  .then(results => res.sendStatus(201))
-  .catch(console.error);
+    .then(results => res.sendStatus(201))
+    .catch(console.error);
 });
 
 app.put('/api/v1/books/:id', bodyParser, (req, res) => {
@@ -112,16 +119,16 @@ app.put('/api/v1/books/:id', bodyParser, (req, res) => {
     UPDATE books
     SET title=$1, author=$2, isbn=$3, image_url=$4, description=$5
     WHERE book_id=$6`,
-    [title, author, isbn, image_url, description, req.params.id]
+  [title, author, isbn, image_url, description, req.params.id]
   )
-  .then(() => res.sendStatus(204))
-  .catch(console.error)
-})
+    .then(() => res.sendStatus(204))
+    .catch(console.error);
+});
 
 app.delete('/api/v1/books/:id', (req, res) => {
   client.query('DELETE FROM books WHERE book_id=$1', [req.params.id])
-  .then(() => res.sendStatus(204))
-  .catch(console.error);
+    .then(() => res.sendStatus(204))
+    .catch(console.error);
 });
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
